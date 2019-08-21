9fa821efbd4416d942177bbcd9be4b2b07958c83
kaigor619 <kaigor619@gmail.com>
2019-08-21 21:06:40 +0300

    yandex-map style

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,18 +2,17 @@ import React from 'react';
 import MyMap from '../myMap';
 import Input from '../input';
 import ListRoute from '../list-route';
-import './app.css';
 
 const App = ()=>{
 
     return(
         <div className="container">  
             <div className="row">
-                <div className='col-6'>
+                <div className='col-md-4 col-5'>
                     <Input />
                     <ListRoute/>
                 </div>
-                <div className="col-6">
+                <div className="col-md-8 col-7">
                     <MyMap/>
                 </div>
             </div>   
